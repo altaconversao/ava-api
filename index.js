91e616fe75b8ce9482a26ac25f3fea2e995eda23
altaconversao <automacoes.altaconversao@gmail.com>
2025-04-03 05:30:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import OpenAI from "openai";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
